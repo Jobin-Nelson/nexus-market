d81157fcf60bc8ceb6f49d5b6ec14aafcbe3f007
jobin_nelson <jobinnelson369@gmail.com>
2026-01-31 22:42:00 +0530

    feat: setup local

---
+++ app/entrypoint.prod.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/bash
+
+echo $'\nRunning in Production...\n'
+
+python3 manage.py collectstatic --noinput
+python3 manage.py migrate --noinput
+python3 -m gunicorn --bind 0.0.0.0:8000 --workers 3 config.wsgi:application
