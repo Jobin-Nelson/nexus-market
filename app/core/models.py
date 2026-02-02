ce3c09a917a98835ad813576b9290beab56b6c76
jobin_nelson <jobinnelson369@gmail.com>
2026-02-02 21:38:04 +0530

    feat: add product models

--- app/core/models.py
+++ app/core/models.py
@@ -1,8 +1,56 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from mptt.models import MPTTModel, TreeForeignKey
 
 # Create your models here.
 
 
 class User(AbstractUser):
     age = models.IntegerField(blank=True, null=True)
+
+
+class Vendor(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+    image = models.ImageField(upload_to='vendors/images', blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Poduct(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+    image = models.ImageField(upload_to='products/images', blank=True, null=True)
+    vendor = models.ForeignKey(Vendor, on_delete=models.CASCADE)
+    regular_price = models.DecimalField(max_digits=10, decimal_places=2)
+    discount_price = models.DecimalField(max_digits=10, decimal_places=2)
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField("Created at", auto_now_add=True, editable=False)
+    updated_at = models.DateTimeField("Updated at", auto_now=True)
+
+    class Meta:
+        ordering = ['-created_at']
+        verbose_name = "Product"
+        verbose_name_plural = "Products"
+
+    def __str__(self):
+        return self.name
+
+
+class Category(MPTTModel):
+    name = models.CharField(max_length=255, unique=True)
+    parent = TreeForeignKey(
+        "self", on_delete=models.CASCADE, null=True, blank=True, related_name='children'
+    )
+    description = models.TextField(null=True, blank=True)
+
+    class MPTTMeta:
+        order_insertion_by = ["name"]
+
+    class Meta:
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
+
+    def __str__(self):
+        return self.name
